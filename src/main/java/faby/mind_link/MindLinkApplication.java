132b6f8acf403e917d4adda8f71769c00db48c96
Fabio Curci <fabio.curci11@gmail.com>
2025-10-21 20:15:58 +0200

    first commit

---
+++ src/main/java/faby/mind_link/MindLinkApplication.java
@@ -0,0 +1,14 @@
+package faby.mind_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindLinkApplication {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO MIND-LINK");
+		SpringApplication.run(MindLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/faby/mind_link/MindLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package faby.mind_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
